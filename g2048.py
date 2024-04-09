98999ef9558549cc854d65d89011c5166f2a6c55
Joseph Calabro <beebfish49@gmail.com>
2024-04-09 01:00:48 -0600

    finished the slide method.

--- g2048.py
+++ g2048.py
@@ -22,11 +22,23 @@ class board2048():
         for i, j in enumerate(self.board):
             if j == 0:
                 indecies.append(i)
+
+        if len(indecies) == 0:
+            return
+        
         self.board[choice(indecies)] = choice((1, 2))
 
     def printBoard(self):
+        print('\n')
+        cleanboard = self.board.copy()
+        boardstr = ''
+        for i in range(len(cleanboard)):
+            if cleanboard[i] == 0:
+                cleanboard[i] = '_'
+            boardstr = boardstr + str(cleanboard[i])
         for i in range(4):
-            print(self.board[i*4:i*4+4])
+            print(boardstr[i*4:i*4+4])
+        print('\n')
 
     def getIndex(self, cord:tuple[int, int]):
         index = 4*cord[1] + cord[0]
@@ -36,16 +48,25 @@ class board2048():
     
     def slide(self, direction):
         directionTuple = self.directions[direction]
-        startingCord = int((directionTuple[0]*-1.5)+1.5)
+        startingCord = int((directionTuple[0]*1.5)+1.5)
         cordList = [[0, 0], [1, 1], [2, 2], [3, 3]]
+
+        latestEmptyList = [-1, -1, -1, -1]
 
         for i in cordList:
             i[directionTuple[1]] = startingCord
         
-        print(cordList)
+        for i in range(4):
+            for j in range(4):
+                if latestEmptyList[j] != -1 and self.board[self.getIndex(cordList[j])] != 0:
+                    self.board[self.getIndex(latestEmptyList[j])] = self.board[self.getIndex(cordList[j])]
+                    self.board[self.getIndex(cordList[j])] = 0
 
+                    latestEmptyList[j][directionTuple[1]] += directionTuple[0]*-1
 
+                if latestEmptyList[j] == -1 and self.board[self.getIndex(cordList[j])] == 0:
+                    latestEmptyList[j] = cordList[j].copy()
+                
+                cordList[j][directionTuple[1]] += directionTuple[0]*-1
 
 board2048_1 = board2048()
-board2048_1.newBlock()
-board2048_1.slide('right')
