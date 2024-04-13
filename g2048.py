cbd1e4d1f0962032f80139560af38d24f43bafed
Joseph Calabro <beebfish49@gmail.com>
2024-04-12 23:46:04 -0600

    made it so that merge and slide can input different boards.

--- g2048.py
+++ g2048.py
@@ -28,9 +28,11 @@ class board2048():
         
         self.board[choice(indecies)] = choice((1, 2))
 
-    def printBoard(self):
+    def printBoard(self, board = -1):
+        if board == -1:
+            board = self.board.copy()
         print('\n')
-        cleanboard = self.board.copy()
+        cleanboard = board.copy()
         boardstr = ''
         for i in range(len(cleanboard)):
             if cleanboard[i] == 0:
@@ -46,7 +48,7 @@ class board2048():
             return index
         return None
     
-    def slide(self, direction):
+    def slide(self, direction, board):
         directionTuple = self.directions[direction]
         startingCord = int((directionTuple[0]*1.5)+1.5)
         cordList = [[0, 0], [1, 1], [2, 2], [3, 3]]
@@ -58,18 +60,19 @@ class board2048():
         
         for i in range(4):
             for j in range(4):
-                if latestEmptyList[j] != -1 and self.board[self.getIndex(cordList[j])] != 0:
-                    self.board[self.getIndex(latestEmptyList[j])] = self.board[self.getIndex(cordList[j])]
-                    self.board[self.getIndex(cordList[j])] = 0
+                if latestEmptyList[j] != -1 and board[self.getIndex(cordList[j])] != 0:
+                    board[self.getIndex(latestEmptyList[j])] = board[self.getIndex(cordList[j])]
+                    board[self.getIndex(cordList[j])] = 0
 
                     latestEmptyList[j][directionTuple[1]] += directionTuple[0]*-1
 
-                if latestEmptyList[j] == -1 and self.board[self.getIndex(cordList[j])] == 0:
+                if latestEmptyList[j] == -1 and board[self.getIndex(cordList[j])] == 0:
                     latestEmptyList[j] = cordList[j].copy()
                 
                 cordList[j][directionTuple[1]] += directionTuple[0]*-1
+        return board
     
-    def merge(self, direction):
+    def merge(self, direction, board):
         directionTuple = self.directions[direction]
         startingCord = int((directionTuple[0]*1.5)+1.5)
         cordList = [[0, 0], [1, 1], [2, 2], [3, 3]]
@@ -81,18 +84,18 @@ class board2048():
             for j in range(4):
                 lookCord = cordList[j].copy()
                 lookCord[directionTuple[1]] += directionTuple[0]*-1
-                workingValues = [self.board[self.getIndex(cordList[j])], self.board[self.getIndex(lookCord)]]
+                workingValues = [board[self.getIndex(cordList[j])], board[self.getIndex(lookCord)]]
                 if workingValues[0] != 0 and workingValues[1] == workingValues[0]:
-                    print(f'got here {workingValues}')
-                    self.board[self.getIndex(cordList[j])] = int(workingValues[1])+1
-                    self.board[self.getIndex(lookCord)] = 0
+                    board[self.getIndex(cordList[j])] = int(workingValues[1])+1
+                    board[self.getIndex(lookCord)] = 0
                 
                 cordList[j] = lookCord
+        return board
     
     def play(self, direction):
-        self.slide(direction)
-        self.merge(direction)
-        self.slide(direction)
+        self.slide(direction, self.board)
+        self.merge(direction, self.board)
+        self.slide(direction, self.board)
         self.newBlock()
 
 board2048_1 = board2048()
